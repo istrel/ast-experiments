d4274abbcb456f2b83f23e04b5468470941760c4
Ivan Strelkov <istrelkov@onlinetours.ru>
2017-10-07 23:12:41 +0300

    Parse html files too

--- src/web-frontend.ts
+++ src/web-frontend.ts
@@ -1,10 +1,10 @@
-declare function require(name: string);
 import * as esprima from 'esprima';
 import * as fs from 'fs';
 import * as path from 'path';
-import * as basis from 'basisjs';
 import * as csstree from 'css-tree';
+import * as basis from 'basisjs';
 import * as child_process from 'child_process';
+import * as htmlparser from 'htmlparser2';
 
 const tokenize = basis.require('basis.template.tokenize');
 
@@ -17,6 +17,31 @@ const actiPath = path.resolve(pathToRoot, 'src/acti');
 // basis.data.value -> /Users/strelkov/work/basisjs/src/basis/data/value.js
 function getAbsolutePath(basisRequire) {
   return basisFolderName + '/' + basisRequire.split('.').slice(1).join('/') + '.js';
+}
+
+function processCssFile(absolutePath: string, filesToVisit) {
+  const fileContents: string = fs.readFileSync(absolutePath, 'utf8');
+  const ast = csstree.parse(fileContents);
+
+  csstree.walk(ast, function(node) {
+    if (node.type === 'Url') {
+      const dirname = path.dirname(absolutePath);
+      let relativePath;
+
+      if (node.value.type === 'Raw') {
+        relativePath = node.value.value;
+      } else if (node.value.type === 'String') {
+        // dirty way to unescape
+        relativePath = eval(node.value.value);
+      } else {
+        throw new Error(`unexpected type of node.value - ${node.value.type}`)
+      }
+
+      const absolutePathToRequiredFile = path.resolve(dirname, relativePath);
+
+      filesToVisit.push(absolutePathToRequiredFile);
+    }
+  });
 }
 
 function processPreset(appRelativePath: string, startFile: string) {
@@ -62,37 +87,41 @@ function processPreset(appRelativePath: string, startFile: string) {
         }
 
         if (requireString[0] === '.') {
-          console.log('RELATIVE: ', requireString);
+          // console.log('RELATIVE: ', requireString);
           const dirname = path.dirname(absolutePath);
           const absolutePathToRequiredFile = path.resolve(dirname, requireString);
           filesToVisit.push(addOptionalJsExtension(absolutePathToRequiredFile));
-        } else if (requireString.substr(0, 6) === 'basis.') {
-          console.log('BASIS. ', requireString);
-          // skip basis
+        } else if (requireString.substr(0, 6) === 'basis.' || requireString === 'socketMessenger') {
+          // console.log('BASIS. ', requireString);
+          // skip libs
         } else if (requireString.substr(0, 4) === 'app.') {
-          console.log('APP. ', requireString);
+          // console.log('APP. ', requireString);
           const fromAppSegment = requireString.substr(4);
           const relative = toRelative(fromAppSegment);
           const absolutePath = path.resolve(appPath, relative);
           filesToVisit.push(absolutePath);
         } else if (requireString.substr(0, 4) === 'app:') {
-          console.log('APP: ', requireString);
+          // console.log('APP: ', requireString);
           const fromAppSegment = requireString.substr(4);
           const absolutePath = path.resolve(appPath, fromAppSegment);
           filesToVisit.push(addOptionalJsExtension(absolutePath));
         } else if (requireString.substr(0, 5) === 'acti.') {
-          console.log('ACTI. ', requireString);
+          // console.log('ACTI. ', requireString);
           const fromActiSegment = requireString.substr(5);
           const relative = toRelative(fromActiSegment);
           const absolutePath = path.resolve(actiPath, relative);
           filesToVisit.push(absolutePath);
         } else if (requireString.substr(0, 5) === 'acti:') {
-          console.log('ACTI: ', requireString);
+          // console.log('ACTI: ', requireString);
           const fromActiSegment = requireString.substr(5);
           const absolutePath = path.resolve(actiPath, fromActiSegment);
           filesToVisit.push(addOptionalJsExtension(absolutePath));
+        } else if (requireString[0] === '/') {
+          const relativeFromRoot = requireString.substr(1);
+          const absolutePath = path.resolve(pathToRoot, relativeFromRoot);
+          filesToVisit.push(absolutePath);
         } else {
-          console.log('WTF: ', requireString);
+          throw new Error('WTF: ' + requireString);
         }
       } else {
         // check for require('basis.l10n').dictionary(...)
@@ -150,7 +179,7 @@ function processPreset(appRelativePath: string, startFile: string) {
         const argCode = fileContents.substring(singleArg.range[0], singleArg.range[1]);
         const dirname = path.dirname(absolutePath);
 
-        const evaledPath = new Function('__dirname', '__filename', `return ${argCode};`)(dirname, absolutePath);
+        const evaledPath = new Function('__dirname', '__filename', `return ${argCode};`)(dirname, absolutePath.replace(/\.js$/, '.l10n'));
 
         // basis:ui paths are exceptions
         if (evaledPath.substr(0, 6) === 'basis:') {
@@ -184,26 +213,26 @@ function processPreset(appRelativePath: string, startFile: string) {
 
             // parsing tmpl files differs a bit, so I copy/pasted it
             if (requireString[0] === '.') {
-              console.log('RELATIVE: ', requireString);
+              // console.log('RELATIVE: ', requireString);
               const dirname = path.dirname(absolutePath);
               const absolutePathToRequiredFile = path.resolve(dirname, requireString);
               filesToVisit.push(absolutePathToRequiredFile);
             } else if (requireString.substr(0, 6) === 'basis.') {
-              console.log('BASIS. ', requireString);
+              // console.log('BASIS. ', requireString);
               // skip basis
             } else if (requireString.substr(0, 4) === 'app.') {
-              console.log('APP. ', requireString);
+              // console.log('APP. ', requireString);
               // skip dotted paths because they are visited in js files
             } else if (requireString.substr(0, 4) === 'app:') {
-              console.log('APP: ', requireString);
+              // console.log('APP: ', requireString);
               const fromAppSegment = requireString.substr(4);
               const absolutePath = path.resolve(appPath, fromAppSegment);
               filesToVisit.push(absolutePath);
             } else if (requireString.substr(0, 5) === 'acti.') {
-              console.log('ACTI. ', requireString);
+              // console.log('ACTI. ', requireString);
               // skip dotted paths because they are visited in js files
             } else if (requireString.substr(0, 5) === 'acti:') {
-              console.log('ACTI: ', requireString);
+              // console.log('ACTI: ', requireString);
               const fromActiSegment = requireString.substr(5);
               const absolutePath = path.resolve(actiPath, fromActiSegment);
               filesToVisit.push(absolutePath);
@@ -281,31 +310,6 @@ function processPreset(appRelativePath: string, startFile: string) {
     }
   }
 
-  function processCssFile(absolutePath: string) {
-    const fileContents: string = fs.readFileSync(absolutePath, 'utf8');
-    const ast = csstree.parse(fileContents);
-
-    csstree.walk(ast, function(node) {
-      if (node.type === 'Url') {
-        const dirname = path.dirname(absolutePath);
-        let relativePath;
-
-        if (node.value.type === 'Raw') {
-          relativePath = node.value.value;
-        } else if (node.value.type === 'String') {
-          // dirty way to unescape
-          relativePath = eval(node.value.value);
-        } else {
-          throw new Error(`unexpected type of node.value - ${node.value.type}`)
-        }
-
-        const absolutePathToRequiredFile = path.resolve(dirname, relativePath);
-
-        filesToVisit.push(absolutePathToRequiredFile);
-      }
-    });
-  }
-
   function getRequiredFiles(absolutePath: string) {
     filesToVisit.push(absolutePath);
 
@@ -326,7 +330,7 @@ function processPreset(appRelativePath: string, startFile: string) {
             processTmplFile(nextFile);
             break;
           case '.css':
-            processCssFile(nextFile);
+            processCssFile(nextFile, filesToVisit);
             break;
           case '.png':
           case '.gif':
@@ -353,6 +357,7 @@ function processPreset(appRelativePath: string, startFile: string) {
 
 const visited = {};
 
+// start parsing from js
 [
   ['src/agent/app', 'src/agent/app/index.js'],
   ['src/agent/app', 'src/agent/remote/index.js'],
@@ -370,8 +375,95 @@ const visited = {};
   for (var key in presetVisited) {
     visited[key] = true;
   }
-}, {})
+}, {});
 
+// parse html files
+[
+  'src/agent/index.html',
+  'src/agent/landing.html',
+  'src/agent/maintenance.html',
+  'src/agent/remote.html',
+  'src/agent/terms-ct.html',
+  'src/agent/terms.html',
+  'src/dealer/index.html',
+  'src/dealer/landing.html',
+  'src/dealer/remote.html',
+  'src/dealer/terms-ct.html',
+  'src/dealer/terms.html'
+].forEach(function(relativePathToHtml) {
+  const absolutePath = path.resolve(pathToRoot, relativePathToHtml);
+
+  visited[absolutePath] = true;
+
+  const fileContents = fs.readFileSync(absolutePath, 'utf8');
+
+  const filesToVisit = [];
+
+  const parser = new htmlparser.Parser({
+    onopentag: function(tagname, attrs) {
+      let relpath = '';
+
+      if (tagname === 'img') {
+        relpath = attrs.src;
+      } else if (tagname === 'link') {
+        relpath = attrs.href;
+      } else if (tagname === 'meta' && attrs.name && attrs.name.match(/^msapplication-.*(logo|TileImage)$/)) {
+        relpath = attrs.content;
+      }
+
+      if (!relpath) {
+        return;
+      }
+
+      if (relpath.substr(0, 4) === 'http') {
+        return;
+      }
+
+      const dirname = path.dirname(absolutePath);
+      const absolutePathToFile = path.resolve(dirname, relpath);
+
+      filesToVisit.push(absolutePathToFile);
+    }
+  });
+
+  parser.write(fileContents);
+  parser.end();
+
+  while (filesToVisit.length > 0) {
+    const nextFile = filesToVisit.pop();
+
+    if (!visited[nextFile]) {
+      visited[nextFile] = true;
+
+      switch (path.extname(nextFile)) {
+        case '.css':
+          processCssFile(nextFile, filesToVisit);
+          break;
+        case '.png':
+        case '.gif':
+        case '.woff':
+        case '.jpg':
+        case '.ico':
+        case '.svg':
+        case '.json':
+          // nothing to do with json and images. Just mark it as visited
+          break;
+        default:
+          throw new Error(`${nextFile} has not valid extension`)
+      }
+    }
+  }
+});
+
+// whitelist some files
+[
+  'src/acti/old-browser/detect.js'
+]
+  .forEach(function(relpath) {
+    const absolutePath = path.resolve(pathToRoot, relpath);
+
+    visited[absolutePath] = true;
+  });
 
 const existing = {};
 
